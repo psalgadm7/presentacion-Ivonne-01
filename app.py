6a119c355047e41fe4179967b27c0285b4d86c15
psalgadm7 <psalgadm7@gmail.com>
2026-05-14 22:05:10 -0400

    feat: commit inicial - aplicacion carrusel Flask

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+LAMINAS_DIR = r"C:\Users\psalg\OneDrive\Imágenes\làminas grupal 2\làminas grupal 2"
+ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif", ".webp"}
+
+
+def get_images():
+    images = []
+    for filename in sorted(os.listdir(LAMINAS_DIR)):
+        ext = os.path.splitext(filename)[1].lower()
+        if ext in ALLOWED_EXTENSIONS:
+            images.append(filename)
+    return images
+
+
+@app.route("/")
+def index():
+    images = get_images()
+    return render_template("index.html", images=images)
+
+
+@app.route("/img/<path:filename>")
+def serve_image(filename):
+    return send_from_directory(LAMINAS_DIR, filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
